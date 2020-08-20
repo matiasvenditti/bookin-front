45e49711ceae8c11c7c4184768a42d1deaf6306e
Matías Gayo <matigayito.mg@gmail.com>
2020-08-19 22:37:52 -0300

    arreglo parcial de la navbar, arreglo del footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import UserStatus from "../UserStatus/UserStatus";
 import AppBar from '@material-ui/core/AppBar'
-import { InputBase, fade, Theme, makeStyles, createStyles } from "@material-ui/core";
+import { InputBase, fade, Theme, makeStyles, createStyles, Toolbar } from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
-import "./Header.module.css";
+import "./Header.css";
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
@@ -24,30 +24,34 @@ export class Header extends React.Component<{}, State>{
         };
         
     }
-
+    
     render(){
       
 
         return(
-            <AppBar position='static' color='primary'>
+            <AppBar position='static' color='primary' className='header'>
+              <Toolbar>
                 <h2 >Book in</h2>
               <div className='search'>
                 <div className='searchIcon'>
                   <SearchIcon />
                 </div>
-                <InputBase
-                  placeholder="Search…"
-                  classes={{
-                    root: 'inputRoot',
-                    input: 'inputInput',
-                  }}
+                  <InputBase
+                    placeholder="Search…"
+                    classes={{
+                      root: 'inputRoot',
+                      input: 'inputInput',
+                    }}
                   inputProps={{ 'aria-label': 'search' }}
                 />
-            </div> 
-            <ButtonGroup variant="contained" color="secondary" aria-label="contained primary button group">
+            </div>
+            <div className='button'> 
+            <ButtonGroup variant="contained" color="secondary" aria-label="contained primary button group" >
               <Button>Log In</Button>
               <Button href='/signup'>Sign Up</Button>
             </ButtonGroup>         
+            </div>
+            </Toolbar>
           </AppBar> 
         )
     }
