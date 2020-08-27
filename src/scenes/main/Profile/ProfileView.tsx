ee701bf8d1f4e6d569f57d9efe7e9ae8922928ba
riccijuanga <riccijuanga@gmail.com>
2020-08-27 16:27:23 -0300

    profile view

--- src/scenes/main/Profile/ProfileView.tsx
+++ src/scenes/main/Profile/ProfileView.tsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
+import { List, ListItem, ListItemAvatar, ListItemText, Divider } from '@material-ui/core'
 
-export default class ProfileView extends Component {
+interface ProfileViewProps {
+    data: {
+        firstName: string,
+        lastName: string,
+        email: string,
+        gender: string,
+        photo: any,
+    }
+}
+
+interface ProfileViewState {
+    data: { key: string, value: string }[],
+}
+
+export default class ProfileView extends Component<ProfileViewProps, ProfileViewState> {
+    constructor(props: ProfileViewProps) {
+        super(props);
+        this.state = {
+            data: [
+                { key: 'Nombre', value: props.data.firstName + ' ' + props.data.lastName },
+                { key: 'Mail', value: props.data.email },
+                { key: 'Género', value: props.data.gender },
+                { key: 'Foto', value: props.data.photo },
+            ]
+        }
+    }
     render() {
+        const { data } = this.state;
         return (
-            <div>
-                <h3>Profile VIEW</h3>
-            </div>
+            <List className='card-container'>
+                {data.map((item, i) => ([
+                    <ListItem>
+                        <ListItemText primary={item.value} secondary={item.key} />
+                    </ListItem>,
+                    (i !== data.length - 1) && <Divider />,
+                ]))}
+            </List>
         )
     }
 }
