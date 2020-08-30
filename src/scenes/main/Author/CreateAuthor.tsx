1cc87b45a4b57ee81f1b2367e1037ff1d4517854
matiasvenditti <mativenditti@gmail.com>
2020-08-30 10:12:55 -0300

    correcciones

--- src/scenes/main/Author/CreateAuthor.tsx
+++ src/scenes/main/Author/CreateAuthor.tsx
@@ -3,20 +3,21 @@ import {Typography} from "@material-ui/core";
 import {NewAuthor} from "../../../model/NewAuthor";
 import AuthorForm from "./AuthorForm";
 import "./CreateAuthor.css"
-import {createAuthor} from "../../../services/AuthorService";
+import {Author, createAuthor} from "../../../services/AuthorService";
+import {AxiosResponse} from "axios";
 
 export default class CreateAuthor extends Component {
      
     handleSubmit = (values: NewAuthor, photo: File) => {
         createAuthor(values, photo)
-            .then(() => console.log("Bien"))
+            .then((response: AxiosResponse<Author>) => console.log(response.data))
             .catch((e) => console.error(e))
     }
 
     render() {
         return (
             <div className='route-container' >
-                <div >
+                <div className='form-container'>
                     <Typography align='center' variant='h5'>Creá un autor</Typography>
                     <AuthorForm onSubmit={this.handleSubmit} />
                 </div>
