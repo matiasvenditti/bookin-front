7be9b110b717dfe6ae20fe9b4f3d5e4a96f5e303
Matías Gayo <matigayito.mg@gmail.com>
2020-09-06 13:57:25 -0300

    agregada la foto del autor 3.0

--- src/scenes/main/Author/ModifyAuthor.tsx
+++ src/scenes/main/Author/ModifyAuthor.tsx
@@ -50,7 +50,7 @@ class ModifyAuthor extends Component<ModifyaAuthorProp, ModifyAuthorState> {
             const photo: string = `data:image/jpeg;base64,${response.data.photo}`;            
             this.setState((prevState: ModifyAuthorState) => ({
                 ...prevState,
-                value: new EditAuthorFormModel(response.data),
+                values: new EditAuthorFormModel(response.data),
                 bytearray: photo,
             }))
         })
