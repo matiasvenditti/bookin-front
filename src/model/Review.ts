43b5adc18caf0b4111987af6824bb748a3614e0d
Pedro Gardelliano <pedro.gardelliano@ing.austral.edu.ar>
2020-10-05 11:16:45 -0300

    Added review tab on profile, just need to update it for title when backend is implemented.

---
+++ src/model/Review.ts
@@ -0,0 +1,5 @@
+export interface Review {
+    id: number,
+    stars: number,
+    comment: string,
+}
--- src/services/ReviewService.ts
+++ src/services/ReviewService.ts
@@ -4,12 +4,17 @@ import {instance} from "../utils/Interceptors/Inerceptors";
 import {baseURL} from "./EnvironmentService";
 import {ReviewWithUser} from "../model/ReviewWithUser";
 import ResponseDelete from "../model/responses/ResponseDelete";
+import {Review} from "../model/Review";
 
 
 class ReviewService {
 
     static getReviews = (bookID: number): Promise<AxiosResponse<ReviewWithUser[]>> => {
         return instance.get<ReviewWithUser[]>(`${baseURL}/books/${bookID}/reviews`)
+    }
+
+    static getReviewsFromUser = (userID: number): Promise<AxiosResponse<Review[]>> => {
+        return instance.get<ReviewWithUser[]>(`${baseURL}/users/${userID}/reviews`)
     }
 
     static deleteReview = (reviewID: number): Promise<AxiosResponse<ResponseDelete>> => {
