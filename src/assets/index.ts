e8fc2fb80574ab46ec8a6923c6f8ffc13aaeb53e
Juan Gabriel Ricci <riccijuanga@gmail.com>
2020-10-27 20:36:37 -0300

    fany images for home ranking by genres buttons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,40 @@
 import dummyAvatar from './dummy-avatar.jpg';
-
+import action from './action.png';
+import adventure from './adventure.png';
+import autorealization from './autorealization.png';
+import biography from './biography.png';
+import children from './children.png';
+import comedy from './comedy.png';
+import drama from './drama.png';
+import education from './education.png';
+import fantasy from './fantasy.png';
+import fiction from './fiction.png';
+import mistery from './mistery.png';
+import novel from './novel.png';
+import poetry from './poetry.png';
+import police from './police.png';
+import romance from './romance.png';
+import selfhelp from './selfhelp.png';
+import terror from './terror.png';
+import tragic from './tragic.png';
 export {
     dummyAvatar,
+    action,
+    adventure,
+    autorealization,
+    biography,
+    children,
+    comedy,
+    drama,
+    education,
+    fantasy,
+    fiction,
+    mistery,
+    novel,
+    poetry,
+    police,
+    romance,
+    selfhelp,
+    terror,
+    tragic,
 }
